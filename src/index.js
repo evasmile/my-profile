326aba0ec565f18749fd5a8b3dc753cd7f24fc59
surprise_Mathonsi <surprisehlawutelo@gmail..com>
2024-07-01 10:28:14 +0200

    add javascript file

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+
+class Skills {
+
+    constructor(name,level,image){
+
+        name=this.name;
+        level=this.level;
+        image=this.image;
+
+    }
+}
+
+const TechnicalComputerSkill=[
+    new Skills("javaScript","Good","download (5).png"),
+    new Skills("Android Studio","Good","download (6).png"),
+    new Skills("MySql","Fair","download (7).png"),
+    new Skills("HTML","Good","download (8).png"),
+    new Skills("Agile Methodology","Fair","download (9).png"),
+    new Skills("CSS","Good","download (10).png"),
+    new Skills("GitHub","Good","Good"),
+    new Skills("Git","Good","download (1).jpg")
+] 
+
+
+
+
+
