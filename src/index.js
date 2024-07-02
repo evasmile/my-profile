f49e9816d7fb7a5ed2cc77ea347f4212aed0816b
surprise_Mathonsi <surprisehlawutelo@gmail..com>
2024-07-02 12:33:07 +0200

    add javascript

--- src/index.js
+++ src/index.js
@@ -4,23 +4,35 @@ class Skills {
 
     constructor(name,level,image){
 
-        name=this.name;
-        level=this.level;
-        image=this.image;
+        this.name=name;
+        this.level=level;
+        this.image=image;
 
     }
 }
 
-const TechnicalComputerSkill=[
-    new Skills("javaScript","Good","download (5).png"),
-    new Skills("Android Studio","Good","download (6).png"),
-    new Skills("MySql","Fair","download (7).png"),
-    new Skills("HTML","Good","download (8).png"),
-    new Skills("Agile Methodology","Fair","download (9).png"),
-    new Skills("CSS","Good","download (10).png"),
-    new Skills("GitHub","Good","Good"),
-    new Skills("Git","Good","download (1).jpg")
-] 
+const TechnicalComputerSkill=[]
+
+TechnicalComputerSkill.push(  new Skills("javaScript","Good","download (5).png"),
+new Skills("Android Studio","Good","download (6).png"),
+new Skills("MySql","Fair","download (7).png"),
+new Skills("HTML","Good","download (8).png"),
+new Skills("Agile Methodology","Fair","download (9).png"),
+new Skills("CSS","Good","download (10).png"),
+new Skills("GitHub","Good","Good"),
+new Skills("Git","Good","download (1).jpg"))
+  
+
+
+$(function(){
+   let grid= $(".TechnicalSkill")
+    for(let skill in TechnicalComputerSkill){
+       $("#name").append(TechnicalComputerSkill[skill].name)
+
+          console.log(TechnicalComputerSkill[skill].name)
+    }
+   
+})
 
 
 
