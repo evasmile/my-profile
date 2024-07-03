715169bd7c6ce46bb2c91d85fa4f4d63e90e68ff
surprise_Mathonsi <surprisehlawutelo@gmail..com>
2024-07-04 00:00:06 +0200

    add more java script

--- src/index.js
+++ src/index.js
@@ -19,24 +19,43 @@ new Skills("MySql","Fair","download (7).png"),
 new Skills("HTML","Good","download (8).png"),
 new Skills("Agile Methodology","Fair","download (9).png"),
 new Skills("CSS","Good","download (10).png"),
-new Skills("GitHub","Good","Good"),
+new Skills("GitHub","Good","download (2).jpg"),
 new Skills("Git","Good","download (1).jpg"))
   
 
 
 $(function(){
-   let grid= $(".TechnicalSkill")
-    for(let skill in TechnicalComputerSkill){
-       $("#name").append(TechnicalComputerSkill[skill].name)
+    const rows = 2;
+    const columns = 4;
 
-          console.log(TechnicalComputerSkill[skill].name)
+    // Get the grid container element
+    const gridContainer = $(".TechnicalSkillGrid");
+    let count =0;
+    // Create the grid dynamically
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        // Create a grid item element
+        let test=`<div class="gridBoxTechnical">
+      <div class="box box1"></div>
+      <div class="box box2"></div>
+      <div class="contentBox">
+        <img class="skillImage" src="./res/${TechnicalComputerSkill[count].image}" alt="powerpoint">
+        <hr>
+        <h3 class="fontSkillCard"> ${TechnicalComputerSkill[count].name} </h3>
+        <p>level : ${TechnicalComputerSkill[count].level}</p>
+      </div>
+    </div>`;
+    count++;
+        // Append the grid item to the grid container
+        gridContainer.append(test);
     }
+  }
    
 })
 
 $(function(){
     const year = new Date().getFullYear();
-    $("#copyRight").text(`Made With Love By Surprise Mathonsi @ ${year}`)
+    $("#copyRight").text(`Made With Love By Surprise Mathonsi |  © ${year}`)
 })
 
 
