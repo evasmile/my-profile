53d079765d1bbb2314c3e11e5807845fea9779f8
surprise_Mathonsi <surprisehlawutelo@gmail..com>
2024-07-14 22:46:08 +0200

    add more to website

--- src/index.js
+++ src/index.js
@@ -84,7 +84,16 @@ $(function(){
 })
 
 
-
+$(function () {
+  $('.emailContactPic').click(function (event) {
+    var email = 'sample@gmail.com';
+    var subject = 'Test';
+    var emailBody = 'Hi Sample,';
+    var attach = 'path';
+    document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody+
+        "?attach="+attach;
+  });
+});
 
 
 
